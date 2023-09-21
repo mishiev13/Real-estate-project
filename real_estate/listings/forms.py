b5a25b4d4f092ea573a22b61c8443a697b8a0753
Albert Mishiev <57565337+mishiev13@users.noreply.github.com>
2023-09-21 14:22:12 +0300

    First Commit

---
+++ real_estate/listings/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Listing
+
+
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = [
+            "title",
+            "price",
+            "num_bedrooms",
+            "num_bathrooms",
+            "square_footage",
+            "address",
+            "image",
+        ]
+
